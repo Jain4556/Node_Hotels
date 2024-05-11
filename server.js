061d7675bd5d866b5c57a2757f3b8cc817d6fed1
Jain4556 <sunil925business@gmail.com>
2024-05-11 21:22:00 +0530

    first version of our hotels

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express()
+const db = require('./db')
+
+const  MenuItem   = require('./person');
+
+app.get('/', function (req, res) {
+  res.send('Hello World, This is sunil')
+})
+
+app.get('/', function (req, res){
+  res.send("Welcome to My new Hotel....")
+})
+
+
+
+// post
+app.post ('/hotel', async (req, res)=>{
+  try{
+    const  data  = req.body
+
+    const newMenu = MenuItem(data);
+
+
+    const response  =await newMenu.save();
+    console.log('data saved');
+    res.status(200).json(response)
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error: 'Internal server error'})
+  }
+
+})
+
+app.get('/hotel', async (req, res)=>{
+  try{
+    const data  = await MenuItem.find();
+    console.log('data fetched');
+    res.status(200).json(data)
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json({error: 'Internal server error'})
+  }
+})
+
+ app.listen(3000, ()=>{
+  console.log("Lisetning on port 3000")
+ })
